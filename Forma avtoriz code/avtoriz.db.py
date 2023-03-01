56894a4424474db533f085bf1d52a6f5f3b35736
Romiz <bmw1972bmw@yandex.ru>
2023-03-01 17:02:40 +0300

    I’ll think about it tomorrow

---
+++ Forma avtoriz code/avtoriz.db.py
@@ -0,0 +1,91 @@
+from tkinter import *
+from tkinter import messagebox as ms
+import sqlite3
+
+with sqlite3.connect('avtoriz.db') as db:
+    c = db.cursor()
+
+c.execute('CREATE TABLE IF NOT EXISTS user (username TEXT NOT NULL PRIMARY KEY,password TEX NOT NULL);')
+db.commit()
+db.close()
+
+class main:
+    def __init__(self, master):
+        self.master = master
+        self.username = StringVar()
+        self.password = StringVar()
+        self.n_username = StringVar()
+        self.n_password = StringVar()
+        self.widgets()
+
+    def login(self):
+            with sqlite3.connect('avtoriz.db') as db:
+                c = db.cursor()
+
+                find_user = ('SELECT * FROM user WHERE username = ? and password = ?')
+                c.execute(find_user, [(self.username.get()), (self.password.get())])
+                result = c.fetchall()
+                if result:
+                    self.logf.pack_forget()
+                    self.head['text'] = self.username.get() + '\n Добрый день'
+                    self.head['pady'] = 150
+                else:
+                    ms.showerror('Уведомление', 'Данный пользователь не найден!')
+
+    def new_user(self):
+            with sqlite3.connect('avtoriz.db') as db:
+                c = db.cursor()
+            find_user = ('SELECT username FROM user WHERE username = ?')
+            c.execute(find_user, [(self.n_username.get())])
+            if c.fetchall():
+                ms.showerror('Уведомление!', 'Имя пользователя занято, Попробуйте другое.')
+            else:
+                ms.showinfo('Успех!', 'Пользователь создан!!!')
+                self.log()
+            insert = 'INSERT INTO user(username,password) VALUES(?,?)'
+            c.execute(insert, [(self.n_username.get()), (self.n_password.get())])
+            db.commit()
+
+    def log(self):
+            self.username.set('')
+            self.password.set('')
+            self.crf.pack_forget()
+            self.head['text'] = 'Авторизация'
+            self.logf.pack()
+
+    def cr(self):
+            self.n_username.set('')
+            self.n_password.set('')
+            self.logf.pack_forget()
+            self.head['text'] = 'Создать пользователя'
+            self.crf.pack()
+
+    def widgets(self):
+            self.head = Label(self.master, text='Авторизация', font=('', 35), pady=10)
+            self.head.pack()
+            self.logf = Frame(self.master, padx=10, pady=10)
+            Label(self.logf, text='Логин: ', font=('', 20), pady=5, padx=5).grid(sticky=W)
+            Entry(self.logf, textvariable=self.username, bd=5, font=('', 15)).grid(row=0, column=1)
+            Label(self.logf, text='Пароль: ', font=('', 20), pady=5, padx=5).grid(sticky=W)
+            Entry(self.logf, textvariable=self.password, bd=5, font=('', 15), show='*').grid(row=1, column=1)
+            Button(self.logf, text=' Войти ', bd=3, font=('', 15), padx=5, pady=5, command=self.login).grid()
+            Button(self.logf, text=' Cоздать пользователя ', bd=3, font=('', 15), padx=5, pady=5, command=self.cr).grid(
+                row=2,
+                column=1)
+            self.logf.pack()
+
+            self.crf = Frame(self.master, padx=10, pady=10)
+            Label(self.crf, text='логин: ', font=('', 20), pady=5, padx=5).grid(sticky=W)
+            Entry(self.crf, textvariable=self.n_username, bd=5, font=('', 15)).grid(row=0, column=1)
+            Label(self.crf, text='Пароль: ', font=('', 20), pady=5, padx=5).grid(sticky=W)
+            Entry(self.crf, textvariable=self.n_password, bd=5, font=('', 15), show='*').grid(row=1, column=1)
+            Button(self.crf, text='Создать пользователя', bd=3, font=('', 15), padx=5, pady=5, command=self.new_user).grid()
+            Button(self.crf, text='Назад', bd=3, font=('', 15), padx=5, pady=5, command=self.log).grid(row=2, column=1)
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Авторизация')
+    main(root)
+    root.mainloop()
+
+
