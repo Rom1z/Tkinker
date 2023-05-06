5f1e21762a707f50e54373acaa666db98ea16ac7
Romiz <119493658+Rom1z@users.noreply.github.com>
2023-05-06 15:25:43 +0300

    Never give up, even if it seems that everything is against you. Strong spirit and perseverance lead to success
    Never give up, even if it seems that everything is against you. Strong spirit and perseverance lead to success

---
+++ Avtoriz+bd/as.sql
@@ -0,0 +1,7 @@
+CREATE TABLE registation (
+    id INT UNSIGNED NOT NULL,
+ empname VARCHAR(255) NOT NULL,
+    mobile VARCHAR(255) NOT NULL,
+    salary VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
